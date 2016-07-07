b4d4963aaffdae24acfadf4697679eb569caaf9b
zxx <zxx@zxxdeMacBook-Pro.local>
2016-07-08 01:56:27 +0800

    add apks

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -17,35 +17,37 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        addCheckAppUpdateEvent();
     }
 };
 
+function addCheckAppUpdateEvent() {
+    var el = document.getElementById('check');
+    el.addEventListener("click", checkAppUpdate);
+
+    var version = document.getElementById('version');
+    version.innerHTML = AppVersion.build;
+}
+
+function checkAppUpdate() {
+    var updateUrl = "https://github.com/vaenow/cordova-plugin-app-update-demo/remote_server_apk/version.xml";
+    window.AppUpdate.checkAppUpdate(onSuccess, onFail, updateUrl);
+    
+    var me = this;
+    function onFail() {console.log('fail', arguments);}
+    function onSuccess() {
+        console.log('success', arguments);
+        me.innerHTML+="<br/>up-to-date";
+    }
+}
+
+
 app.initialize();
+
